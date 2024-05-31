635d0395b86e703b2b094830d4c772754182d84a
ylianst <ylianst@gmail.com>
2024-05-31 04:29:30 -0700

    More code

--- components/iq2020/iq2020_climate.h
+++ components/iq2020/iq2020_climate.h
@@ -6,7 +6,7 @@
 namespace esphome {
 namespace iq2020_climate {
 
-	class IQ2020Climate : public climate_::Climate, public Component {
+	class IQ2020Climate : public climate::Climate, public Component {
 	public:
 		void setup() override;
 		void write_state(bool state) override;
