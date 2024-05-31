c0fd40574033d2a2abc9201411a8d3720ec6df4f
ylianst <ylianst@gmail.com>
2024-05-30 20:45:39 -0700

    More code

--- components/iq2020/binary_sensor.py
+++ components/iq2020/binary_sensor.py
@@ -2,6 +2,8 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import binary_sensor
 from esphome.const import (
+    DEVICE_CLASS_LIGHT,
+    DEVICE_CLASS_PRESENCE,
     DEVICE_CLASS_CONNECTIVITY,
     ENTITY_CATEGORY_DIAGNOSTIC,
 )
