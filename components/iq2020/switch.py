a1641eec5988626fc8144d1c0f0db8fcbe04de8d
ylianst <ylianst@gmail.com>
2024-05-31 01:34:13 -0700

    More code

--- components/iq2020/switch.py
+++ components/iq2020/switch.py
@@ -8,8 +8,10 @@ from . import ns, IQ2020Component
 CONF_IQ2020_SERVER = "iq2020_server"
 CONF_SWITCH_LIGHTS = "iq2020_switch"
 
-iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
-EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
+#iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
+#EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
+
+EmptySwitch = ns.class_('IQ2020Switch', switch.Switch, cg.Component)
 
 CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(EmptySwitch)
@@ -20,5 +22,5 @@ async def to_code(config):
     await cg.register_component(server, config)
     await switch.register_switch(server, config)
 
-    var = await cg.get_variable(config[CONF_IQ2020_SERVER])
-    cg.add(server.set_request_mod(config[CONF_SWITCH_LIGHTS]))
+#    var = await cg.get_variable(config[CONF_IQ2020_SERVER])
+#    cg.add(server.set_request_mod(config[CONF_SWITCH_LIGHTS]))
