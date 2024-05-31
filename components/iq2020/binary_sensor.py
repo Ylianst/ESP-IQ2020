ad642fef067b6159ff3a13c0d59081246edf33d0
ylianst <ylianst@gmail.com>
2024-05-30 20:46:27 -0700

    More code

--- components/iq2020/binary_sensor.py
+++ components/iq2020/binary_sensor.py
@@ -21,13 +21,13 @@ CONFIG_SCHEMA = cv.Schema(
             device_class=DEVICE_CLASS_CONNECTIVITY,
             entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
         ),
-        cv.Required(CONF_SENSOR_LIGHTS): binary_sensor.binary_sensor_schema(
-            device_class=DEVICE_CLASS_LIGHT,
-            icon=ICON_LIGHTBULB,
-        ),
         cv.Required(CONF_SENSOR_CONNECTIONKIT): binary_sensor.binary_sensor_schema(
             device_class=DEVICE_CLASS_PRESENCE,
             entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        ),
+        cv.Required(CONF_SENSOR_LIGHTS): binary_sensor.binary_sensor_schema(
+            device_class=DEVICE_CLASS_LIGHT,
+            icon=ICON_LIGHTBULB,
         ),
     }
 )
