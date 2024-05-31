4518c76eff01f4c966669a795a670384b3e9db79
ylianst <ylianst@gmail.com>
2024-05-30 17:59:34 -0700

    More code.

--- components/climate/__init__.py
+++ components/climate/__init__.py
@@ -38,7 +38,7 @@ CONFIG_SCHEMA = cv.All(
 #    .extend(CLIENT_SCHEMA)
 )
 
-async def to_code(config):
+#async def to_code(config):
 #    var = cg.new_Pvariable(config[CONF_ID])
 #    await cg.register_component(var, config)
 #    await climate.register_climate(var, config)
