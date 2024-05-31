b04bd614e0c8af056ce40f0f8632aa8a5cc38423
ylianst <ylianst@gmail.com>
2024-05-31 04:16:32 -0700

    More code

--- components/iq2020/climate.py
+++ components/iq2020/climate.py
@@ -1,19 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import switch
+from esphome.components import climate
 from esphome.const import CONF_ID
 
 CONF_IQ2020_SERVER = "IQ2020Component"
 CONF_CLIMATE = "iq2020_climate"
 
 iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_climate')
-IQ2020Climate = iq2020_switch_ns.class_('IQ2020Climate', switch.Switch, cg.Component)
+IQ2020Climate = iq2020_switch_ns.class_('IQ2020Climate', climate.Switch, cg.Component)
 
-CONFIG_SCHEMA = switch.CLIMATE_SCHEMA.extend({
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(IQ2020Climate)
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
     server = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(server, config)
-    await switch.register_switch(server, config)
+    await climate.register_climate(server, config)
