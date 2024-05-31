a0b92dde04578eb27667ce51b68d28b22cb66d0b
ylianst <ylianst@gmail.com>
2024-05-31 01:35:07 -0700

    More code

--- components/iq2020/switch.py
+++ components/iq2020/switch.py
@@ -8,10 +8,10 @@ from . import ns, IQ2020Component
 CONF_IQ2020_SERVER = "iq2020_server"
 CONF_SWITCH_LIGHTS = "iq2020_switch"
 
-#iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
-#EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
+iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
+EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
 
-EmptySwitch = ns.class_('IQ2020Switch', switch.Switch, cg.Component)
+#EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
 
 CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(EmptySwitch)
