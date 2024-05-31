d4609477f5be8d1d41a9b708ccaa6cde787198d4
ylianst <ylianst@gmail.com>
2024-05-31 01:52:45 -0700

    More code

--- components/iq2020/switch.py
+++ components/iq2020/switch.py
@@ -11,16 +11,22 @@ CONF_SWITCH_LIGHTS = "iq2020_switch"
 iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
 EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
 
-#EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
-
 CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(EmptySwitch)
 }).extend(cv.COMPONENT_SCHEMA)
 
-async def to_code(config):
-    server = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(server, config)
-    await switch.register_switch(server, config)
+#async def to_code(config):
+#    server = cg.new_Pvariable(config[CONF_ID])
+#    await cg.register_component(server, config)
+#    await switch.register_switch(server, config)
 
-    var = await cg.get_variable(config[CONF_IQ2020_SERVER])
-    cg.add(var.set_light_switch(server))
+async def to_code(config):
+    var = await switch.new_switch(config)
+    await cg.register_component(var, config)
+
+    paren = await cg.get_variable(config[CONF_ECONET_ID])
+    cg.add(var.set_econet_parent(paren))
+    cg.add(var.set_request_mod(config[CONF_REQUEST_MOD]))
+    cg.add(var.set_request_once(config[CONF_REQUEST_ONCE]))
+    cg.add(var.set_switch_id(config[CONF_SWITCH_DATAPOINT]))
+    cg.add(var.set_src_adr(config[CONF_SRC_ADDRESS]))
