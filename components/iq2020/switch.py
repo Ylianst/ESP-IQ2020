6dba7e83f258555aa6741b3bac57b86c035391a3
ylianst <ylianst@gmail.com>
2024-05-31 22:00:50 -0700

    More code

--- components/iq2020/switch.py
+++ components/iq2020/switch.py
@@ -5,6 +5,7 @@ from esphome.const import CONF_ID
 
 from . import ns, IQ2020Component
 
+CONF_IQ2020_ID = "IQ2020Component";
 CONF_IQ2020_SERVER = "iq2020_server"
 CONF_SWITCH_LIGHTS = "iq2020_switch"
 
@@ -19,3 +20,7 @@ async def to_code(config):
     server = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(server, config)
     await switch.register_switch(server, config)
+
+    paren = await cg.get_variable(config[CONF_IQ2020_ID])
+    cg.add(var.set_iq2020_parent(paren))
+    cg.add(var.set_switch_id(config[CONF_SWITCH_DATAPOINT]))
