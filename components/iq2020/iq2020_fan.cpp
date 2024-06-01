888e24fb840fde0a24f5d004d82b17b05f012162
ylianst <ylianst@gmail.com>
2024-06-01 16:05:28 -0700

    More code

--- components/iq2020/iq2020_fan.cpp
+++ components/iq2020/iq2020_fan.cpp
@@ -10,10 +10,10 @@ namespace iq2020_fan {
 	static const char *TAG = "iq2020_fan.fan";
 
 	void IQ2020Fan::setup() {
-		/*
 		if (fan_id < FANCOUNT) { g_iq2020_fan[fan_id] = this; }
 		ESP_LOGD(TAG, "Fan:%d Setup", fan_id);
 
+		/*
 		auto traits = fan::FanTraits();
 		traits.set_direction(false);
 		traits.set_oscillation(false);
