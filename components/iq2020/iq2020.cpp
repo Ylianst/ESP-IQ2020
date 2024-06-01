d0f4a80719c67c18ce865256d5f8a38b96825c59
ylianst <ylianst@gmail.com>
2024-05-31 23:54:17 -0700

    Added version string to DataViewer

--- components/iq2020/iq2020.cpp
+++ components/iq2020/iq2020.cpp
@@ -383,7 +383,7 @@ void IQ2020Component::SwitchAction(unsigned int switchid, int state) {
 		case SWITCH_SPALOCK: {
 			switch_pending[SWITCH_SPALOCK] = state;
 			if (state != 0) {
-				unsigned char cmd[] = { 0x0B, 0x1D, (state != 0) ? 0x02 : 0x01 };
+				unsigned char cmd[] = { 0x0B, 0x1D, 0x02 };
 				sendIQ2020Command(0x01, 0x1F, 0x40, cmd, sizeof(cmd)); // Turn on spa lock
 			}
 			else {
