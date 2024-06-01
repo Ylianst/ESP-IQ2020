1aaed7ebafbcd4507fc0b1fb2b9e8a4c05981020
ylianst <ylianst@gmail.com>
2024-06-01 16:52:17 -0700

    More code

--- components/iq2020/iq2020_fan.cpp
+++ components/iq2020/iq2020_fan.cpp
@@ -20,11 +20,12 @@ namespace iq2020_fan {
 	}
 
 	esphome::fan::FanTraits IQ2020Fan::get_traits() {
-		auto traits = fan::FanTraits();
-		traits.set_direction(false);
-		traits.set_oscillation(false);
-		traits.set_speed(true);
-		traits.speed_count(2);
+		auto traits = fan::FanTraits(false, true, false, 2);
+		//FanTraits(bool oscillation, bool speed, bool direction, int speed_count)
+		//traits.set_direction(false);
+		//traits.set_oscillation(false);
+		//traits.set_speed(true);
+		//traits.speed_count(2);
 		return traits;
 	};
 
