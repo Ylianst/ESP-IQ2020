dea827dc36a8596947962ed559499b450f11db72
ylianst <ylianst@gmail.com>
2024-06-01 15:25:07 -0700

    More code

--- components/iq2020/iq2020_fan.h
+++ components/iq2020/iq2020_fan.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/output/binary_output.h"
+//#include "esphome/components/output/binary_output.h"
 #include "esphome/components/fan/fan_state.h"
 
 DEPENDENCIES = ["binary_output"]
@@ -12,14 +12,14 @@ namespace iq2020_fan {
 class IQ2020Fan : public Component {
  public:
   void set_fan(fan::FanState *fan) { fan_ = fan; }
-  void set_output(output::BinaryOutput *output) { output_ = output; }
+  //void set_output(output::BinaryOutput *output) { output_ = output; }
   void setup() override;
   void loop() override;
   void dump_config() override;
   
  protected:
   fan::FanState *fan_;
-  output::BinaryOutput *output_;
+//  output::BinaryOutput *output_;
   bool next_update_{true};
 };
 
