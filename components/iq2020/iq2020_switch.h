eb004037df06c28033452750a3968737b4bbf52e
ylianst <ylianst@gmail.com>
2024-05-31 22:07:06 -0700

    More code

--- components/iq2020/iq2020_switch.h
+++ components/iq2020/iq2020_switch.h
@@ -11,6 +11,10 @@ namespace iq2020_switch {
 		void setup() override;
 		void write_state(bool state) override;
 		void dump_config() override;
+		void set_switch_id(const std::string &switch_id) { this->switch_id_ = switch_id; }
+
+	protected:
+		std::string switch_id_;
 	};
 
 } //namespace iq2020_switch
