33fde92f3bb85dea38ec5a0b94b41f38b5286f0e
ylianst <ylianst@gmail.com>
2024-06-01 16:36:28 -0700

    More code

--- components/iq2020/iq2020_fan.h
+++ components/iq2020/iq2020_fan.h
@@ -2,14 +2,13 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/fan/fan.h"
-#include "iq2020.h"
 
 namespace esphome {
 namespace iq2020_fan {
 
-	class IQ2020Fan : public fan_::Fan, public Component {
+	class IQ2020Fan : public fan::Fan, public Component {
 	public:
-		void set_fan(fan::Fan *fan) { fan_ = fan; }
+//		void set_fan(fan::Fan *fan) { fan_ = fan; }
 		//void set_output(output::BinaryOutput *output) { output_ = output; }
 		void setup() override;
 //		void loop() override;
@@ -17,7 +16,7 @@ namespace iq2020_fan {
 		void set_fan_id(unsigned int id) { this->fan_id = id; }
 
 	protected:
-		fan::Fan *fan_;
+//		fan::Fan *fan_;
 		//  output::BinaryOutput *output_;
 		bool next_update_{ true };
 		unsigned int fan_id;
