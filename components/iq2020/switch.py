c3c2d47981ee494fe1c273c0db229d07f73b67f5
ylianst <ylianst@gmail.com>
2024-05-31 22:21:32 -0700

    More code

--- components/iq2020/switch.py
+++ components/iq2020/switch.py
@@ -9,12 +9,26 @@ CONF_IQ2020_ID = "IQ2020Component";
 CONF_IQ2020_SERVER = "iq2020_server"
 CONF_SWITCH_LIGHTS = "iq2020_switch"
 
-iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
-EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
+#iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
+#EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
 
-CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(EmptySwitch)
-}).extend(cv.COMPONENT_SCHEMA)
+#CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
+#    cv.GenerateID(): cv.declare_id(EmptySwitch)
+#}).extend(cv.COMPONENT_SCHEMA)
+
+IQ2020Switch = ens.class_(
+    "IQ2020Switch", switch.Switch, cg.Component
+)
+
+CONFIG_SCHEMA = (
+    switch.switch_schema(IQ2020Switch)
+    .extend(
+        {
+            cv.Required(CONF_SWITCH_DATAPOINT): cv.string,
+        }
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
     server = cg.new_Pvariable(config[CONF_ID])
