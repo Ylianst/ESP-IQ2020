cea0f228d57fe2172f9cb47a6eaba92096738eb7
ylianst <ylianst@gmail.com>
2024-05-31 22:25:54 -0700

    More code

--- components/iq2020/switch.py
+++ components/iq2020/switch.py
@@ -9,26 +9,14 @@ CONF_IQ2020_ID = "IQ2020Component";
 CONF_IQ2020_SERVER = "iq2020_server"
 CONF_SWITCH_LIGHTS = "iq2020_switch"
 
-#iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
-#EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
+iq2020_switch_ns = cg.esphome_ns.namespace('iq2020_switch')
+EmptySwitch = iq2020_switch_ns.class_('IQ2020Switch', switch.Switch, cg.Component)
 
-#CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
-#    cv.GenerateID(): cv.declare_id(EmptySwitch)
-#}).extend(cv.COMPONENT_SCHEMA)
-
-IQ2020Switch = ens.class_(
-    "IQ2020Switch", switch.Switch, cg.Component
-)
-
-CONFIG_SCHEMA = (
-    switch.switch_schema(IQ2020Switch)
-    .extend(
-        {
-            cv.Required(CONF_SWITCH_DATAPOINT): cv.string,
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-)
+CONFIG_SCHEMA = switch.SWITCH_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(EmptySwitch)
+})
+.extend(cv.COMPONENT_SCHEMA)
+.extend({ cv.Required(CONF_SWITCH_DATAPOINT): cv.string, })
 
 async def to_code(config):
     server = cg.new_Pvariable(config[CONF_ID])
@@ -39,4 +27,3 @@ async def to_code(config):
 #    cg.add(server.set_iq2020_parent(paren))
 #   cg.add(server.set_switch_id(config[CONF_ID]))
     cg.add(server.set_switch_id(config["name"]))
-#    cg.add(server.set_switch_id(config[CONF_SWITCH_DATAPOINT]))
