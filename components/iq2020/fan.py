68af6790301bea0d0640e7cc66744bd418081db9
ylianst <ylianst@gmail.com>
2024-06-01 15:47:28 -0700

    More code

--- components/iq2020/fan.py
+++ components/iq2020/fan.py
@@ -1,33 +1,33 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import fan, output
-from esphome.const import CONF_DIRECTION_OUTPUT, CONF_OSCILLATION_OUTPUT, CONF_OUTPUT, CONF_OUTPUT_ID
+from esphome.const import CONF_ID
 #from .. import iq2020_fan_ns
 
 from . import ns, IQ2020Component
 
 CONF_FAN_DATAPOINT = "fan_datapoint"
 
-IQ2020Fan = iq2020_fan_ns.class_('iq2020_fan', cg.Component)
+#IQ2020Fan = iq2020_fan_ns.class_('iq2020_fan', cg.Component)
 
-#iq2020_fan_ns = cg.esphome_ns.namespace('iq2020_fan')
-#IQ2020Fan = iq2020_fan_ns.class_('IQ2020Fan', fan.Fan, cg.Component)
+iq2020_fan_ns = cg.esphome_ns.namespace('iq2020_fan')
+IQ2020Fan = iq2020_fan_ns.class_('IQ2020Fan', fan.Fan, cg.Component)
 
 CONFIG_SCHEMA = fan.FAN_SCHEMA.extend({
-    cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(IQ2020Fan),
+    cv.GenerateID(CONF_ID): cv.declare_id(IQ2020Fan),
 #    cv.Required(CONF_OUTPUT): cv.use_id(output.BinaryOutput),
 #    cv.Optional(CONF_DIRECTION_OUTPUT): cv.use_id(output.BinaryOutput),
 #    cv.Optional(CONF_OSCILLATION_OUTPUT): cv.use_id(output.BinaryOutput),
 }).extend({ cv.Required(CONF_FAN_DATAPOINT): cv.positive_int }).extend(cv.COMPONENT_SCHEMA)
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
 
     fan_ = yield fan.create_fan_state(config)
     cg.add(var.set_fan(fan_))
-    output_ = yield cg.get_variable(config[CONF_OUTPUT])
-    cg.add(var.set_output(output_))
+#    output_ = yield cg.get_variable(config[CONF_OUTPUT])
+#    cg.add(var.set_output(output_))
 
 #    if CONF_OSCILLATION_OUTPUT in config:
 #        oscillation_output = yield cg.get_variable(config[CONF_OSCILLATION_OUTPUT])
