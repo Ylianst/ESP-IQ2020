2d8e414e3346dc82ff8e61a476e9c88d68f7a519
ylianst <ylianst@gmail.com>
2024-06-02 14:40:27 -0700

    More code

--- components/iq2020/text_sensor.py
+++ components/iq2020/text_sensor.py
@@ -1,9 +1,10 @@
 ﻿import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import sensor
+from esphome.components import text_sensor
 from . import ns, IQ2020Component
 
 CONF_IQ2020_SERVER = "iq2020_server"
+CONF_SENSOR_VERSION = "version"
 
 CONFIG_SCHEMA = cv.Schema(
     {
@@ -13,3 +14,4 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def to_code(config):
     server = await cg.get_variable(config[CONF_IQ2020_SERVER])
+	sens = await text_sensor.new_binary_sensor(config[CONF_SENSOR_VERSION])
