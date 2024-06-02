1c100f4166e172dc83059a6c531ae0ae7fc94b5f
ylianst <ylianst@gmail.com>
2024-06-02 14:49:59 -0700

    More code

--- components/iq2020/text_sensor.py
+++ components/iq2020/text_sensor.py
@@ -9,7 +9,10 @@ CONF_SENSOR_VERSION = "version"
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.GenerateID(CONF_IQ2020_SERVER): cv.use_id(IQ2020Component),
-        cv.Optional(CONF_SENSOR_VERSION): text_sensor.text_sensor_schema()
+        cv.Optional(CONF_SENSOR_VERSION): text_sensor.text_sensor_schema(
+            device_class=DEVICE_CLASS_CONNECTIVITY,
+            entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
+        )
     }
 )
 
@@ -17,5 +20,5 @@ async def to_code(config):
     server = await cg.get_variable(config[CONF_IQ2020_SERVER])
     
     if CONF_SENSOR_VERSION in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_SENSOR_VERSION])
-        cg.add(server.set_connected_sensor(sens))
+        sens = await text_sensor.new_text_sensor(config[CONF_SENSOR_VERSION])
+#        cg.add(server.set_connected_sensor(sens))
