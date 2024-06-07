864b439e4bfc45f1e105e0fb2068b022c9e975d5
ylianst <ylianst@gmail.com>
2024-06-06 23:31:48 -0700

    Added power on counter.

--- components/iq2020/sensor.py
+++ components/iq2020/sensor.py
@@ -198,4 +198,3 @@ async def to_code(config):
     if CONF_SENSOR_POWER_ON_COUNTER in config:
         sens = await sensor.new_sensor(config[CONF_SENSOR_POWER_ON_COUNTER])
         cg.add(server.set_power_on_counter_sensor(sens))
-		//
