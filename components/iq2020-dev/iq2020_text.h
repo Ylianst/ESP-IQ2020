7394a4ebda64617e3800a80348bb66a700cd03ce
ylianst <ylianst@gmail.com>
2024-06-21 15:30:46 -0700

    Dev

--- components/iq2020-dev/iq2020_text.h
+++ components/iq2020-dev/iq2020_text.h
@@ -12,7 +12,10 @@ namespace iq2020_text {
 		void control(const std::string &value) override;
 		void dump_config() override;
 		void set_text_id(unsigned int id) { this->text_id = id; }
-		void set_value(const std::string &value) { this->text_value = value; }
+		void set_value(const std::string &value) {
+			this->text_value = value;
+			this->publish_state(value);
+		}
 		std::string text_value;
 
 	protected:
