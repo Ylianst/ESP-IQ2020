ce9eb0154aeb87ac49da2cdb1f918ee932518c74
ylianst <ylianst@gmail.com>
2024-06-21 11:20:13 -0700

    Dev

--- components/iq2020-dev/iq2020_switch.h
+++ components/iq2020-dev/iq2020_switch.h
@@ -9,7 +9,7 @@ namespace iq2020_switch {
 	class IQ2020Switch : public switch_::Switch, public Component {
 	public:
 		void setup() override;
-		//void write_state(bool state) override;
+		void write_state(bool state) override;
 		void dump_config() override;
 		void set_switch_id(unsigned int id) { this->switch_id = id; }
 
