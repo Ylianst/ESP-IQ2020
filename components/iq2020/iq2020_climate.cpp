0a19c40201052a6b66977d7a0d48b54bb2579b2f
ylianst <ylianst@gmail.com>
2025-04-18 15:25:49 -0700

    80F temp fix

--- components/iq2020/iq2020_climate.cpp
+++ components/iq2020/iq2020_climate.cpp
@@ -61,8 +61,8 @@ namespace iq2020_climate {
 			traits.set_visual_target_temperature_step(0.5);
 			traits.set_visual_current_temperature_step(0.5);
 		} else { // Fahrenheit setup
-			traits.set_visual_min_temperature(fahrenheit_to_celsius(80));
-			traits.set_visual_max_temperature(fahrenheit_to_celsius(104));
+			traits.set_visual_min_temperature(26.5); // fahrenheit_to_celsius(80)
+			traits.set_visual_max_temperature(40); // fahrenheit_to_celsius(104)
 			traits.set_visual_target_temperature_step(1);
 			traits.set_visual_current_temperature_step(1);
 		}
