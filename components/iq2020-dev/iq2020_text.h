6da2805dbfcf30f7a41910dd25948a90768c87d0
ylianst <ylianst@gmail.com>
2025-06-13 00:09:14 -0700

    Text sensor fix

--- components/iq2020-dev/iq2020_text.h
+++ components/iq2020-dev/iq2020_text.h
@@ -37,10 +37,7 @@ namespace iq2020_text {
 		void control(const std::string &value);
 		void dump_config();
 		void set_text_id(unsigned int id) { this->text_id = id; }
-		void set_value(const std::string &value) {
-			this->text_value = value;
-			this->publish_state(value);
-		}
+		void set_value(const std::string &value) { }
 		std::string text_value;
 
 	protected:
