7b4b4a59836946f0463993e7fdc9151d5a0cff5d
ylianst <ylianst@gmail.com>
2025-06-13 00:38:11 -0700

    Text sensor fix

--- components/iq2020-dev/iq2020_text.h
+++ components/iq2020-dev/iq2020_text.h
@@ -26,10 +26,24 @@ namespace iq2020_text {
 } //namespace iq2020_text
 } //namespace esphome
 #else
+
+#include "esphome/core/component.h"
+
 namespace esphome {
 namespace iq2020_text {
 
-	class IQ2020Text { };
+	class IQ2020Text : public text::Text, public Component {
+	public:
+		void setup();
+		void control(const std::string &value);
+		void dump_config();
+		void set_text_id(unsigned int id) { }
+		void set_value(const std::string &value) { }
+		std::string text_value;
+
+	protected:
+		unsigned int text_id;
+	};
 
 } //namespace iq2020_text
 } //namespace esphome
